542e263b6f3542352ef89cd29b38099aa1a2a59f
hridyansh07 <anshkhatri7@gmail.com>
2022-07-15 13:48:24 +0530

    Trying Env with Serverless

--- api/test.ts
+++ api/test.ts
@@ -1,6 +1,6 @@
 import type { VercelRequest, VercelResponse } from '@vercel/node';
 
 export default (request: VercelRequest, response: VercelResponse) => {
-  const  name = "Testing";
+  const  name = process.env.TELEGRAM_URL;
   response.status(200).send(`Hello ${name}!`);
 };
